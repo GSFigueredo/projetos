1ba18465b694000d6f8ed750eebeceea54fde463
gabriel.figueredo <gabriel.sfigueredo8@gmail.com>
2025-07-27 15:55:52 -0300

    criação da lógica de sessão na dashboard principal

--- emily_decoracoes/dashboard_principal/index.js
+++ emily_decoracoes/dashboard_principal/index.js
@@ -1,7 +1,27 @@
 import { verificarLogin } from '../autenticador/index.js';
 
 function usuarioLogado(user) {
-  alert(`usuario logado ${user.nome}`)
+    //alert(`usuario logado ${user.nome}`);
+
+    let linkLogin = $("#link_login");
+    let linkCadastro = $("#link_cadastro");
+
+    linkLogin.attr({
+      "href": 'index.html',
+    })
+    linkLogin.text(`Olá, ${user.nome}`)
+
+    linkCadastro.attr({
+      "href": 'index.html',
+      "class": 'nav-link sair'
+    })
+    linkCadastro.text(`SAIR`)
+
+    linkCadastro.on('click', evento => {
+      evento.preventDefault(); // não permitir que faça evento padrão da tag, exemplo: link, não permite que ele redirecione
+      localStorage.clear();
+      window.location.href = 'index.html';
+    });
 }
 
 // Assim que a página for carregada, chama a função verificarLogin
