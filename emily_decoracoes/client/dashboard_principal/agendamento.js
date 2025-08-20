ad7f4397b17dbcdbaea5469ae57c8d08b65bfdd7
gabriel.figueredo <gabriel.sfigueredo8@gmail.com>
2025-08-19 23:23:13 -0300

    Organização das pastas do front end, dentro da pasta client

--- emily_decoracoes/client/dashboard_principal/agendamento.js
+++ emily_decoracoes/client/dashboard_principal/agendamento.js
@@ -1,5 +1,5 @@
-import * as autenticador from '../backend/autenticador/index.js';
-import { mostrarModal } from '../global/modal/modal.js'
+import * as autenticador from '../../backend/autenticador/index.js';
+import { mostrarModal } from '../../global/modal/modal.js'
 
 $('#btn_agendar').click(validarDados);
 
